61f0723895752a6d67c702e0a311cdf32fd893d4
Shaid Islam <shaid85@gmail.com>
2023-12-29 02:20:53 +0600

    User Model and db config added

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL!)
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully');           
+        })
+
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MOngoDB is running. '+ err);
+            process.exit();
+        })
+    } catch (error) {
+        console.log('Something goes wrong');
+        console.log(error);        
+    }
+}
