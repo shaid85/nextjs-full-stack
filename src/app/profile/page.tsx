ade2a3d883f82a71546e72b890a9b21c77169588
Shaid Islam <shaid85@gmail.com>
2023-12-29 17:19:44 +0600

    getdata from token

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 import axios from "axios";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 
 export default function ProfilePage() {
     const router = useRouter()
+    const [userData, setUserData] = useState("nothing")
     const logout = async () => {
         try {
             await axios.get('/api/users/logout')
@@ -17,14 +20,26 @@ export default function ProfilePage() {
             toast.error(error.message);
         }
     }
+
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        console.log(res.data);
+        setUserData(res.data.data._id)   
+    }
     return (
         <div className="w-full">
             <h1>Profile</h1> 
             <hr />
             <p>Profile Page</p>
+            <h2 className="p-1 rounded bg-green-500">{userData === "nothing" ? "Nothing" : <Link href={`/profile/${userData}`} >{userData}</Link>}</h2>
             <button className=""
             onClick={logout}
             >Logout</button>
+
+            <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >GetUser Details</button>
         </div>
     )
 }
