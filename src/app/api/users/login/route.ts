add41ec5585815b7994b7f8ede4aabd2959e4a0b
Shaid Islam <shaid85@gmail.com>
2024-01-12 18:48:20 +0600

    gmail smtp add

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -42,7 +42,6 @@ export async function POST(request: NextRequest) {
         })
         response.cookies.set("token", token, {
             httpOnly: true,
-            sameSite:false,
         })
         
         return response;
